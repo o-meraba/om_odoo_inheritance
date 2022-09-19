3b33fce205bc2dfd71b3faa5c0ded688edd0f1ec
omeraba <omeraba93@gmail.com>
2022-09-19 12:00:09 +0300

    added field and inherited function

--- __manifest__.py
+++ __manifest__.py
@@ -20,12 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/sale_order_view.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
---
+++ models/sale_order.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    confirmed_user_id = fields.Many2one('res.users', string='Confirmed User')
+
+    def action_confirm(self):
+        super(SaleOrder, self).action_confirm()
+        print("Success........................")
+        self.confirmed_user_id = self.env.user.id
