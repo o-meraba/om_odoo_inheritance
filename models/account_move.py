626df9ef8e972958941828dda7817cf28d5240c0
omeraba <omeraba93@gmail.com>
2022-10-16 03:05:13 +0300

    added and inherited one2many field

--- models/account_move.py
+++ models/account_move.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from email.policy import strict
 from odoo import models, fields, api
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     so_confirmed_user_id = fields.Many2one('res.users', string='SO Confirmed User')
+    
+    
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+    
+    line_number = fields.Integer(string="Line Number")
     
